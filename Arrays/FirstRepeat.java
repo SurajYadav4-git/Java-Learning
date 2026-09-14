00f17299dadf156195e4e12d065d529f2c37565c
SurajYadav4-git <surajyadav4feb@gmail.com>
2026-09-14 14:24:09 +0530

    find first repeated elementin Array

---
+++ Arrays/FirstRepeat.java
@@ -0,0 +1,28 @@
+// find first repeated elementin Array 
+import java.util.*;
+class FirstRepeat{
+
+   static int repeatfirst(int arr[]){
+       for(int i=0; i<arr.length; i++){
+           for(int j=i+1;j<arr.length; j++){
+               if(arr[i]==arr[j]){
+                   return arr[i];
+               }
+          }
+       }
+       return -1;
+   }
+    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr []= new int[n];
+        
+     System.out.print("Enter "+ n + " Elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("First Repeated element:"+ repeatfirst(arr));
+         }
+}
